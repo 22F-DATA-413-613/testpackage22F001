70f7875a3b67b0cc52fbc8200740c60b0c1a6e94
Richard Ressler <rressler@american.edu>
2022-09-27 19:40:57 -0400

    updated help

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,14 @@
+#' Title
+#'
+#' @param a A factor
+#' @param b A factor
+#' @importFrom ggplot2 ggplot aes geom_bar coord_flip
+#' @importFrom rlang .data
+#' @return A factor with combined levels from a and b
+#' @export
+#'
+#' @examples
+#' fbind(as.factor(c("dog", "cat")), as.factor(c("gerbil", "parakeet")))
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
